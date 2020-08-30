0302da87fbf811bd904b09722daa0f953d456a37
theXliner <66848002+ThatXliner@users.noreply.github.com>
2020-08-29 22:16:58 -0700

    Updated __init__.py

--- src/package_name/__init__.py
+++ src/package_name/__init__.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Author: Bryan Hu .
+
+@Bryan Hu .
+
+Made with love by Bryan Hu .
+
+
+Version: v0.1
+
+Desc: Some __init__.py
+
+"""
+
 __version__ = "0.1.0"
