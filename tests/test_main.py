9134f21af55a8fa790ab8978572dd357bd69eb6b
ThatXliner <66848002+ThatXliner@users.noreply.github.com>
2020-09-01 08:12:36 -0700

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -15,7 +15,7 @@ import pytest  # noqa
 from sys import path
 from pathlib import Path
 path.insert(0, str(Path(Path(Path(__file__).parent).parent / "src")))
-from package_name import example_api_func, __version__  # noqa
+from package_name import __version__  # noqa
 
 
 class TestClass(object):
