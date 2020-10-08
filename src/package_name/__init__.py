65af1aa33f87d2869b53ab7bbdd3701e0d30ba54
ThatXliner <66848002+ThatXliner@users.noreply.github.com>
2020-10-08 14:18:39 -0700

    Update __init__.py
    [skip ci]

--- src/package_name/__init__.py
+++ src/package_name/__init__.py
@@ -9,10 +9,11 @@ from typing import List
 
 __title__: str = "package_name"  # A unique name avalible in Pypi
 __version__: str = "0.1.0"  # VERSION_BUMP_ANCHOR
-__author__: str = (
-    "Bryan Hu <bryan.hu.2020@gmail.com>"  # The initial author of this project
-)
 __contact__: str = "bryan.hu.2020@gmail.com"  # The email of the initial author
+__author__: str = (  # The initial author of this project
+    f"Bryan Hu <{__contact__}>"  # NOTE: THE FORMAT SHOULD ALWAYS BE "FIRST LAST <EMAIL ADDRESS>"
+)
+
 # The short description that will be shown on Pypi
 __description__: str = "Short description"
 __credits__: List[str] = []  # Other contributors
