90309e8e6e8571325e6cc00bf25ca05eff1f3e3b
ThatXliner <66848002+ThatXliner@users.noreply.github.com>
2020-10-08 13:34:10 -0700

    Update __init__.py
    [skip ci]

--- src/package_name/__init__.py
+++ src/package_name/__init__.py
@@ -7,11 +7,12 @@ Long description.
 """
 from typing import List
 
-__title__: str = "package_name"
-__version__: str = "0.1.0"
+__title__: str = "package_name"  # A unique name avalible in Pypi
+__version__: str = "0.1.0"  # VERSION_BUMP_ANCHOR
 __author__: str = (
     "Bryan Hu <bryan.hu.2020@gmail.com>"  # The initial author of this project
 )
-__contact__: str = "bryan.hu.2020@gmail.com"
+__contact__: str = "bryan.hu.2020@gmail.com"  # The email of the initial author
+# The short description that will be shown on Pypi
 __description__: str = "Short description"
-__credits__: List[str] = []
+__credits__: List[str] = []  # Other contributors
