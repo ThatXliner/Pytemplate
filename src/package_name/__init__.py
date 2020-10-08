47d85af3368df680d178fc5a4a69997a70c18e2a
ThatXliner <66848002+ThatXliner@users.noreply.github.com>
2020-10-08 12:52:32 -0700

    Update __init__.py
    [skip ci]

--- src/package_name/__init__.py
+++ src/package_name/__init__.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Shorter than README description
+Package summary.
+
+Long description.
 """
 from typing import List
 
+__title__: str = "package_name"
 __version__: str = "0.1.0"
-__author__: List[str] = ["Bryan Hu <bryan.hu.2020@gmail.com>"]
+__author__: str = "Bryan Hu <bryan.hu.2020@gmail.com>"  # The initial author of this project
+__contact__: str = 'bryan.hu.2020@gmail.com'
+__description__: str = 'Short description'
+__credits__: List[str] = []
