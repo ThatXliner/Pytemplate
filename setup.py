44e8a586440c04e979330749d0421c81ec673191
ThatXliner <66848002+ThatXliner@users.noreply.github.com>
2020-10-08 14:32:55 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -44,11 +44,13 @@ CLASSIFIERS = [
     # "Intended Audience :: System Administrators",
     # "Topic :: Utilities",
 ]
+_EMAIL_REGEX: str = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
+
 setup(
     name=__title__,
     version=__version__,
     author=search(
-        r"(\w+ \w+) <([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)>", __author__
+        r"(\w+ \w+) <"+_EMAIL_REGEX+">", __author__
     )[1],
     author_email=__contact__,
     description=__description__,
