5ad452979cbeac920c9201f05be6a4036aa8b93b
ThatXliner <66848002+ThatXliner@users.noreply.github.com>
2020-10-19 08:15:55 -0700

    Fix tests for new file structure (#8)
    * Added __all__ var
    
    * Updated tests for new file structure
    
    This *should* work now
    
    * Rename __main__.pyi to sub_module.pyi
    
    [skip ci]
    
    * Update __init__.py

--- src/package_name/__init__.py
+++ src/package_name/__init__.py
@@ -6,6 +6,19 @@ Package summary.
 Long description.
 """
 from typing import List
+from .sub_module import *  # noqa
+
+
+# sub_module is required to have an __all__ variable declared
+# The reason for this is that the package root directory __init__.py
+# should not declare an __all__ variable due to code style.
+# It would make it easier to maintain: nobody wants to update the
+# __init__.py every single time there is a new conponent.
+
+# But, from sub_module import * should only be used if the
+# implementation is split across files. Otherwise, we do nothing
+# The reason for this is that if it makes sense to categorize
+# module-level APIs into different files.
 
 __title__: str = "package_name"  # The name of the package
 __version__: str = "0.1.0"  # VERSION_BUMP_ANCHOR
